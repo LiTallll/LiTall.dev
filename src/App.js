72a64bad02f512155fde50d031688d77cff74d5c
LiTallll <alistair.lewin@live.fr>
2022-05-04 17:47:20 +0200

    Site in construction

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Site in construction, come back later !</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/LiTallll"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my GitHub
         </a>
       </header>
     </div>
