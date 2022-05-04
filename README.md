e02d8fefdf07a874ce1e6cc07af62f51a8539f33
LiTallll <alistair.lewin@live.fr>
2022-05-04 17:57:56 +0200

    updated infos

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # LiTall.dev
-WebSite in Construction, portfolio
-
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+WebSite in construction, portfolio made in reactJS and hosted on Firebase Hosting
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Site in construction, come back later !</p>
+        <p>Website in construction, come back later !</p>
         <a
           className="App-link"
           href="https://github.com/LiTallll"
