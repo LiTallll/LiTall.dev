3e1ca7909534cfdf3ac5d646bc197530b2332c75
LiTallll <alistair.lewin@live.fr>
2022-05-18 20:11:02 +0200

    Added Description

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,30 +1,47 @@
 import "./experience.css";
 
 const experience = () => {
-  return <div>
-    <h1>Experience</h1>
-    <h2>Trace my path to becoming a developer</h2>
-    <div className="Column">
-      <TimeBlock title="Master's degree in Computer Science" subtitle="SUPINFO" start="Sep 2017" end="Oct 2022"/>
-      <TimeBlock title="Baccalaureat" subtitle="Lycée Guy de Maupassant" start="Sep 2014" end="Jul 2017"/>
+  return (
+    <div>
+      <h1>Experience</h1>
+      <h2>Trace my path to becoming a developer</h2>
+      <div className="Column">
+        <TimeBlock
+          title="Master's degree in Computer Science"
+          subtitle="SUPINFO"
+          start="Sep 2017"
+          end="Oct 2022"
+        />
+        <TimeBlock
+          title="Baccalaureat"
+          subtitle="Lycée Guy de Maupassant"
+          start="Sep 2014"
+          end="Jul 2017"
+          description="Grade: Mention BienGrade: Mention Bien<br/>Spécialité Informatique et Sciences du Numérique<br/>Section Européenne<br/>Option Sport"
+        />
+      </div>
     </div>
-    
-  </div>;
+  );
 };
 
-const TimeBlock = ({title, titleLink, subtitle, start, end, description}) => {
-  return <div className="timeBlock">
-    <h3 className="dates">
-      <h6>From</h6>{start}<br/><h6>to</h6>{end}
-      <span className="dot"></span>
-    </h3>
-    <div className="infos">
-      
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <p>{description}</p>
+const TimeBlock = ({ title, subtitle, start, end, description }) => {
+  return (
+    <div className="timeBlock">
+      <div className="dates">
+        <h6>From </h6>
+        <h3>{start}</h3>
+        <br />
+        <h6>to </h6>
+        <h3>{end}</h3>
+        <span className="dot"></span>
+      </div>
+      <div className="infos">
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{description}</p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default experience;
